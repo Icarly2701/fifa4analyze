e20985215faa1b689487d62581c7f59501f213c4
YuSungLee <icarly1379@naver.com>
2023-05-29 21:27:06 +0900

    시작

---
+++ fifaback/recordsearch/src/main/java/fo4/recordsearch/RecordsearchApplication.java
@@ -0,0 +1,13 @@
+package fo4.recordsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordsearchApplication.class, args);
+	}
+
+}
