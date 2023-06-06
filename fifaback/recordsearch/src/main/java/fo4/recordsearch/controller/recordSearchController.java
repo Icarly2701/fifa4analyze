4bc52aba16870b339c5e6c3589185a2c9cc0c654
Lee JaeHyuk <plzgiveme45@gmail.com>
2023-06-07 00:36:56 +0900

    feat: Get User's Best Division

--- fifaback/recordsearch/src/main/java/fo4/recordsearch/controller/recordSearchController.java
+++ fifaback/recordsearch/src/main/java/fo4/recordsearch/controller/recordSearchController.java
@@ -98,6 +98,8 @@ public class recordSearchController {
         getRecordInfo(userInfo);
         getMatchInfo(userInfo.getMatchId().get(0));
 
+        getBestDivisionInfo();
+
     }
 
     private HttpHeaders getHttpHeaders() {
@@ -176,6 +178,69 @@ public class recordSearchController {
         return uri;
     }
 
+    private void getBestDivisionInfo() throws ParseException {
+        URI uri = getBestDivisionRecordidUri();
+
+        //header 생성
+        HttpHeaders httpHeaders = getHttpHeaders();
+
+        // 데이터 가져오기
+        HttpEntity<?> requestMessage = new HttpEntity<>("", httpHeaders);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, requestMessage, String.class);
+
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(responseEntity.getBody());
+
+        JSONArray matchDivisionRecord = (JSONArray) obj;
+        JSONObject officialMatchDivisionRecord = (JSONObject) matchDivisionRecord.get(0);
 
 
+        Long userBestDivisionId = (Long) officialMatchDivisionRecord.get("division");
+        int temp = userBestDivisionId.intValue();
+
+        StringBuilder sb = new StringBuilder();
+
+        switch (temp){
+            case 3100 : sb.append("유망주3"); break;
+            case 3000 : sb.append("유망주2"); break;
+            case 2900 : sb.append("유망주1"); break;
+
+            case 2800 : sb.append("세미프로3"); break;
+            case 2700 : sb.append("세미프로2"); break;
+            case 2600 : sb.append("세미프로1"); break;
+
+            case 2500 : sb.append("프로3"); break;
+            case 2400 : sb.append("프로3"); break;
+            case 2300 : sb.append("프로3"); break;
+
+            case 2200 : sb.append("월드클래스3"); break;
+            case 2100 : sb.append("월드클래스2"); break;
+            case 2000 : sb.append("월드클래스1"); break;
+
+            case 1300 : sb.append("챌린지3"); break;
+            case 1200 : sb.append("챌린지2"); break;
+            case 1100 : sb.append("챌린지1"); break;
+
+            case 1000 : sb.append("슈퍼챌린지"); break;
+            case 900 : sb.append("챔피언스"); break;
+            case 800 : sb.append("슈퍼챔피언스"); break;
+        }
+
+        String userBestDivision = sb.toString();
+
+
+        log.info("officialMatchDivisionRecord = {}", userBestDivision);  // Long 타입
+
+
+    }
+
+    private URI getBestDivisionRecordidUri() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://api.nexon.co.kr/fifaonline4/v1.0")
+                .path("/users/" + accessId + "/maxdivision")
+                .encode()
+                .build()
+                .toUri();
+        return uri;
+    }
 }
